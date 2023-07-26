443974a977c1836710267f96a52555b73480f0bf
SuadHus <suadreal@gmail.com>
2023-07-26 15:21:22 +0200

    finally something

--- household/test.py
+++ household/test.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 from datetime import datetime
 
-star_period = '2023-07-08'
-end_period = '2023-07-12'
+star_period = '2023-07-06'
+end_period = '2023-07-08'
 
 # make relative path for the repo 
 hourly_data = pd.read_csv(r'C:\Users\suad\Desktop\programming\PowerSaveMe\hourly_data.csv', index_col=0, parse_dates=True)
@@ -36,32 +36,72 @@ class House:
         self.WINDOW_TO_WALL_RATIO = 0.2  # Ratio of window area to wall area
     
 
-    def calculate_total_hourly_consumption(self, time, price, outside_temperature, wind_speed, cloudiness):
+    def calculate_total_hourly_consumption(self, time, price, outside_temperature, wind_speed, cloudiness, daily_average_price):
         self.hourly_consumptions[time]= self.calculate_hourly_heating_consumption(outside_temperature, wind_speed, cloudiness) + sum([device.calculate_consumption(time.hour) for device in self.devices])
 
         ##############################
         # optimization block 
 
-        if(self.optimize and self.battery):
-            if(self.hourly_consumptions[time] < 0):
-                if(self.battery.is_full() is not True):
-                    self.battery.charge(-self.hourly_consumptions[time])
-                    self.hourly_consumptions[time] = 0
-            if(self.hourly_consumptions[time] > 0):
-                if(self.battery.charge_level > self.hourly_consumptions[time]):
-                    self.battery.discharge(self.hourly_consumptions[time])
-                    self.hourly_consumptions[time] = 0
+        #if the price of the electricity is higher then average price and the solar power is enough to cover the consumption and if the battery has excess power then sell all the power to the grid
+        #if the price of the electricity is higher then average price and the solar power is not enough to cover the consumption and if the battery has excess power use the battery to cover the consumption and sell the excess if there is any
+        #if the price of the electricity is lower then average price and the solar power is enough to cover the consumption and if the battery has excess power then cover the consumption with it
+        #if the price of the electricity is much cheaper <0.5 and the battery is not full then charge the battery
+        #if the price of the electricity is much cheaper <0.5 and the battery is full then sell the excess power to the grid
+
+        if(self.optimize and self.battery!=None):
+            if(self.hourly_consumptions[time] <= 0): 
+                if(daily_average_price <= price):
+                    if(self.battery.charge_level > 0):
+                        self.hourly_consumptions[time] -= min(2,self.battery.charge_level)
+                        self.battery.discharge(min(2,self.battery.charge_level)) 
+
+
+                if(daily_average_price*0.7 > price):
+                    #charge the battery if the prices are low 
+                    if(self.battery.charge_level < self.battery.capacity):
+                        self.hourly_consumptions[time] += min(2,self.battery.capacity - self.battery.charge_level)
+                        self.battery.charge(min(2,self.battery.capacity - self.battery.charge_level))   
+
+                elif(daily_average_price > price):
+                    if(self.battery.charge_level <= self.battery.capacity):
+                        
+                        if(self.battery.charge_level  - self.hourly_consumptions[time] > self.battery.capacity):
+                            self.hourly_consumptions[time] += min(2,self.battery.capacity - self.battery.charge_level)
+                            self.battery.charge(min(2,self.battery.capacity - self.battery.charge_level))
+                            
+                        else:
+                            self.battery.charge(min(2,-self.hourly_consumptions[time]))
+                            self.hourly_consumptions[time] += min(2,-self.hourly_consumptions[time]) 
+            
+
+            elif(self.hourly_consumptions[time] > 0):
+
+                #use battery if there some energy
+                if(daily_average_price*0.9 <= price):
+                    if(self.battery.charge_level > 0):
+                        self.hourly_consumptions[time] -= min(2,self.battery.charge_level, self.hourly_consumptions[time])
+                        self.battery.discharge(min(2,self.battery.charge_level, self.hourly_consumptions[time]))
+                #use battery but give out less energy if the prices are not that high
+                elif(daily_average_price*0.7 <= price):
+                    if(self.battery.charge_level > 0):
+                        self.hourly_consumptions[time] -= min(1,self.battery.charge_level, self.hourly_consumptions[time])
+                        self.battery.discharge(min(1,self.battery.charge_level, self.hourly_consumptions[time]))
+                # charge otherwise
                 else:
-                    self.hourly_consumptions[time] -= self.battery.discharge(self.hourly_consumptions[time])
+                    if(self.battery.charge_level < self.battery.capacity):
+                        self.hourly_consumptions[time] += min(2,self.battery.capacity - self.battery.charge_level)
+                        self.battery.charge(min(2,self.battery.capacity - self.battery.charge_level))
+
+                    
+
 
         ##############################
-        
-
+    
         self.hourly_consumption_price[time] = self.hourly_consumptions[time]  * price
         self.total_consumption += self.hourly_consumptions[time]         
         self.total_consumption_price += self.hourly_consumption_price[time]
     
-
+    
     def calculate_hourly_heating_consumption(self, outside_temperature, wind_speed, cloudiness):
         if(outside_temperature >= self.target_temperature):
             return 0
@@ -108,13 +148,7 @@ class House:
         # Convert energy from kJ to kWh
         return energy_needed / 3600
 
-        # print(energy_needed / 3600)
-        # self.hourly_consumption_price[time] = self.hourly_consumptions[time]  * price
-        # print(price)
-        # self.total_consumption += self.hourly_consumptions[time]
-        # self.total_consumption_price += self.hourly_consumption_price[time]
 
-    
     def add_battery(self, battery):
         self.battery = battery
     
@@ -122,7 +156,7 @@ class House:
         self.devices.append(device)
     
     def get_total_consumption_price(self):
-        print(self.total_consumption_price)
+        return self.total_consumption_price
 
     def get_hourly_consumption_price(self):
         print(self.hourly_consumption_price)
@@ -152,28 +186,6 @@ class House:
         for device in self.devices:
             device.get_hourly_usage() 
     
-    
-
-class Simulation:
-    def __init__(self, start_date, end_date, hourly_data):
-        self.houses = []
-        self.start_date = start_date
-        self.end_date = end_date
-        self.hourly_data = hourly_data.loc[start_date:end_date]
-        self.optimizer = None
-    
-    def run_simulation(self):
-        for house in self.houses:
-            for index,row in self.hourly_data.iterrows():
-                house.calculate_total_hourly_consumption(index, row['Price']/100, row['Temperature'], row['Wind speed'], row['Cloudiness'])
-
-            # Perform any necessary actions with the total consumption
-            
-        if self.optimizer:
-            self.optimizer.optimize_consumption()
-    
-    
-
     
 
 
@@ -206,7 +218,6 @@ class Device:
     
     
     
-
 class Solar_Panel(Device):
     def __init__(self, name, power_rating):
         super().__init__(name, power_rating)
@@ -250,28 +261,78 @@ class Battery():
 
 #######################
 
+class Simulation:
+    def __init__(self, start_date, end_date, hourly_data):
+        self.houses = []
+        self.start_date = start_date
+        self.end_date = end_date
+        self.hourly_data = hourly_data.loc[start_date:end_date]
+        self.optimizer = None
+        self.randomlst= []
+    
+    def run_simulation(self):
+        for house in self.houses:
+            for index,row in self.hourly_data.iterrows():
+                house.calculate_total_hourly_consumption(index, row['Price']/100, row['Temperature'], row['Wind speed'], row['Cloudiness'], self.calculate_daily_average_price(index))
+                self.randomlst.append(row['Price']/100)
+
+            # Perform any necessary actions with the total consumption
+            
+    
+    def calculate_daily_average_price(self, index):
+        next_day = index + pd.DateOffset(days=1)
+        return self.hourly_data.loc[index:next_day]['Price'].mean()/100
+        
+###############################
+
 # Example usage:
 house1 = House("House 1", 120, "electric", 0.9, 0.8, True)
+house2= House("House 2", 120, "electric", 0.9, 0.8, False)
+
 device1 = Device("Device 1", 100.0)  # Example usage pattern [0.5, 0.5, 0.5] for 3 hours
 device2 = Device("Device 2", 50.0)  # Example usage pattern [0.2, 0.3, 0.1] for 3 hours
+
+house2.add_device(Device("Device 3", 100.0))
+house2.add_device(Device("Device 4", 50.0))
+
 house1.add_device(device1)
 house1.add_device(device2)
 house1.add_device(Solar_Panel("Solar Panel", 100.0))  
 house1.add_device(Solar_Panel("Solar Panel 2", 300.0))
 house1.add_device(Solar_Panel("Solar Panel 3", 500.0))
 
+house2.add_device(Solar_Panel("Solar Panel", 100.0))
+house2.add_device(Solar_Panel("Solar Panel 4", 300.0))
+house2.add_device(Solar_Panel("Solar Panel 5", 500.0))
 
 house1.add_battery(Battery("Battery 1", 10.0))
 
 simulation = Simulation(star_period, end_period, hourly_data)
 simulation.houses.append(house1)
+simulation.houses.append(house2)
 simulation.run_simulation()
 
 
 
-house1.get_power_usage_time_period('2023-07-8 00:00:00', '2023-07-12 23:00:00')
 
-house1.get_all_devices_hourly_usage()
+house1.get_hourly_consumptions()
+
 house1.get_total_consumption()
+print("price for total consumption House 1"+ str(house1.get_total_consumption_price()))
 
 house1.battery.get_battery_level()
+
+
+print("##############################################")
+house2.get_hourly_consumptions()
+
+house2.get_total_consumption()
+print("price for total consumption House 2"+ str(house2.get_total_consumption_price()))
+
+print("##############################################")
+
+# house1.get_all_devices_hourly_usage()
+# house1.get_total_consumption()
+
+# house1.battery.get_battery_level()
+
