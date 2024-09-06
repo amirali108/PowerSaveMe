a16cac77c14a88a31571f40fa523168e10fbb4f5
Amirali Fatehi <117827939+amirali108@users.noreply.github.com>
2024-09-06 21:41:17 +0200

    Update Logo.js

--- solar_ui/src/components/Logo/Logo.js
+++ solar_ui/src/components/Logo/Logo.js
@@ -1,20 +1,20 @@
-
 /**
- * if you want to use image instead of <svg> uncomment following.
+ * If you want to use an image instead of <svg>, uncomment the following lines.
  *
  * import logoDark from 'assets/images/logo-dark.svg';
  * import logo from 'assets/images/logo.svg';
- *
  */
 
 // ==============================|| LOGO SVG ||============================== //
 
 const Logo = () => {
-
   return (
     <>
-    <img src="https://powersave.me/wp-content/uploads/2023/02/HQ2-1024x195.png" alt="Power save me" width="100" />
-
+      <img
+        src="https://powersave.me/wp-content/uploads/2023/02/HQ2-1024x195.png"
+        alt="Power save me"
+        width="100"
+      />
     </>
   );
 };
