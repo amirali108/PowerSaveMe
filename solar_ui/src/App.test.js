f16f8dd4ef65479a61f82d3d9174d4dfad8cccfe
Amirali Fatehi <117827939+amirali108@users.noreply.github.com>
2024-09-06 21:18:20 +0200

    Update App.test.js

--- solar_ui/src/App.test.js
+++ solar_ui/src/App.test.js
@@ -3,18 +3,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-
-// Create a mock store (or you can use your actual store if you want to test real Redux interactions)
-const mockStore = configureStore([]);
-const store = mockStore({
-  // Provide the initial state of your store
-  menu: { drawerOpen: false }
-});
+import store from './path-to-your-redux-store'; // Import the real Redux store
 
 test('renders learn react link', () => {
   render(
-    <Provider store={store}>
+    <Provider store={store}> {/* Use the real Redux store */}
       <MemoryRouter>
         <App />
       </MemoryRouter>
