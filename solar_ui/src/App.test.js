2042316401390f3db2e9f4bb42e0137131c072e0
Amirali Fatehi <117827939+amirali108@users.noreply.github.com>
2024-09-06 21:03:35 +0200

    Update App.test.js

--- solar_ui/src/App.test.js
+++ solar_ui/src/App.test.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter to simulate routing in the test
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+// Create a mock store (or you can use your actual store if you want to test real Redux interactions)
+const mockStore = configureStore([]);
+const store = mockStore({
+  // Provide the initial state of your store
+  menu: { drawerOpen: false }
+});
 
 test('renders learn react link', () => {
   render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
+    <Provider store={store}>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </Provider>
   );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
